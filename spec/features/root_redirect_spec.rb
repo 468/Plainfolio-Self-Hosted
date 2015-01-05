a1f3e7bb2eb379d00a706760e640fd1991c6aaae
Alexander Taylor <a@alexandertaylor.org>
2015-01-05 19:30:30 +0000

    Readme

--- spec/features/root_redirect_spec.rb
+++ spec/features/root_redirect_spec.rb
@@ -9,12 +9,6 @@ feature "Homepage visit" do
 	let!(:admin2) { FactoryGirl.create(:admin) }
   let!(:portfolio) { FactoryGirl.create(:portfolio, admin: admin) }
 
-    scenario "should redirect to account page if signed in with portfolio" do
-  	  sign_in(admin.username,admin.password)
-      visit root_path
-      expect(page).to have_text("Your Portfolio")
-    end
-
     scenario "should redirect to  account page if signed in without portfolio" do
       admin.portfolio.destroy
   	  sign_in(admin.username,admin.password)
